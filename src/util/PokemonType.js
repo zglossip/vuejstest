04db566a828750bb6dfba2beee0353a13e35fbb2
ZACH GLOSSIP <zglossip@drury.edu>
2019-11-28 11:03:32 -0600

    Fix type effectiveness

--- src/util/PokemonType.js
+++ src/util/PokemonType.js
@@ -38,14 +38,14 @@ export const PokemonTypeDefaults = {
   rock: new PokemonType({ fire: 0.5, flying: 0.5, normal: 0.5, poison: 0.5, fighting: 2, grass: 2, ground: 2, steel: 2, water: 2 }),
   bug: new PokemonType({ fighting: 0.5, grass: 0.5, ground: 0.5, fire: 2, flying: 2, rock: 2 }),
   ghost: new PokemonType({ bug: 0.5, poison: 0.5, dark: 2, ghost: 2, normal: 0, fighting: 0 }),
-  steel: new PokemonType({ bug: 0.5, dark: 0.5, dragon: 0.5, flying: 0.5, ghost: 0.5, grass: 0.5, ice: 0.5, normal: 0.5, psychic: 0.5, rock: 0.5, steel: 0.5, fighting: 2, fire: 2, ground: 2, poison: 0 }),
+  steel: new PokemonType({ bug: 0.5, dragon: 0.5, fairy: 0.5, flying: 0.5, grass: 0.5, ice: 0.5, normal: 0.5, psychic: 0.5, rock: 0.5, steel: 0.5, fighting: 2, fire: 2, ground: 2, poison: 0 }),
   fire: new PokemonType({ bug: 0.5, fairy: 0.5, fire: 0.5, grass: 0.5, ice: 0.5, steel: 0.5, ground: 2, rock: 2, water: 2 }),
   water: new PokemonType({ fire: 0.5, ice: 0.5, steel: 0.5, water: 0.5, electric: 2, grass: 2 }),
-  grass: new PokemonType({ electric: 0.5, grass: 0.5, ground: 0.5, bug: 2, fire: 2, flying: 2, ice: 2, poison: 2 }),
+  grass: new PokemonType({ electric: 0.5, grass: 0.5, ground: 0.5, water: 0.5, bug: 2, fire: 2, flying: 2, ice: 2, poison: 2 }),
   electric: new PokemonType({ electric: 0.5, flying: 0.5, steel: 0.5, ground: 2 }),
   psychic: new PokemonType({ fighting: 0.5, psychic: 0.5, bug: 2, dark: 2, ghost: 2 }),
   ice: new PokemonType({ ice: 0.5, fighting: 2, fire: 2, rock: 2, steel: 2 }),
-  dragon: new PokemonType({ electric: 0.5, fire: 0.5, grass: 0.5, water: 0.5, dragon: 2, ice: 2 }),
+  dragon: new PokemonType({ electric: 0.5, fire: 0.5, grass: 0.5, water: 0.5, dragon: 2, fairy: 2, ice: 2 }),
   dark: new PokemonType({ dark: 0.5, ghost: 0.5, bug: 2, fairy: 2, fighting: 2, psychic: 0 }),
   fairy: new PokemonType({ bug: 0.5, dark: 0.5, fighting: 0.5, poison: 2, steel: 2, dragon: 0 })
 }
